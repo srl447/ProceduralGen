8bbd91f006b10f351410e3aac8964a5b5ad2b4df
Unknown <srl447@nyu.edu>
2017-03-27 23:00:36 -0400

    Everything

---
+++ Assets/Scripts/Pathmaker.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathmaker : MonoBehaviour {
+
+    int counter = 0;
+    public Transform floorPrefab;
+    public Transform floorPrefab2;
+    public Transform floorPrefab3;
+    Transform floorPrefabUse;
+    public Transform pathmakerSphereTransform;
+    float maxCounter;
+    float distance;
+
+    void Start()
+    {
+        maxCounter = Mathf.Floor(Random.Range(60f, 80f));
+        distance = Random.Range(3.5f, 5f);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        float floorNum = Random.Range(0f, 2.5f);
+        if (floorNum < 1.2)
+        {
+            floorPrefabUse = floorPrefab;
+        }
+        else if (floorNum < 2)
+        {
+            floorPrefabUse = floorPrefab2;
+        }
+        else
+        {
+            floorPrefabUse = floorPrefab3;
+        }
+
+
+        if(counter < maxCounter && TileCount.tileCount < 500)
+        {
+            float rand = Random.Range(0f, 1f);
+            if(rand < .11)
+            {
+                transform.Rotate(0f, 90f, 0f);
+            }
+            else if (rand < .22)
+            {
+                transform.Rotate(0f, -90f, 0f);
+            }
+            else if (rand > .9)
+            {
+                Instantiate(pathmakerSphereTransform, transform.position, Quaternion.identity);
+            }
+            Instantiate(floorPrefabUse, transform.position, Quaternion.identity);
+            TileCount.tileCount++;
+            transform.Translate(Vector3.forward * distance);
+            counter++;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/TextChanger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChanger : MonoBehaviour {
+
+    Text text;
+    string firstWord;
+    string secondWord;
+	// Use this for initialization
+	void Start ()
+    {
+        text = GetComponent<Text>();
+        float rand = Mathf.Floor(Random.Range(1f, 6f));
+        if(rand == 1)
+        {
+            firstWord = "The";
+        }
+        else if (rand == 2)
+        {
+            firstWord = "Super";
+        }
+        else if (rand == 3)
+        {
+            firstWord = "Extrodinarily";
+        }
+        else if (rand == 4)
+        {
+            firstWord = "Extremely";
+        }
+        else if (rand == 5)
+        {
+            firstWord = "Amazingly";
+        }
+        float rand2 = Mathf.Floor(Random.Range(1f, 6f));
+        if (rand2 == 1)
+        {
+            secondWord = "Awesome";
+        }
+        else if (rand2 == 2)
+        {
+            secondWord = "Fantastic";
+        }
+        else if (rand2 == 3)
+        {
+            secondWord = "Fandoodledistic";
+        }
+        else if (rand2 == 4)
+        {
+            secondWord = "Happy";
+        }
+        else if (rand2 == 5)
+        {
+            secondWord = "Inspiring";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = firstWord + " " + secondWord + " Procedural Generation System" + "\n" + "Press R to restart";
+	}
+}
